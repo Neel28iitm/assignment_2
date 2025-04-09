0e9fd94d9aff198c5a22d029d6986a3fe4d639a5
Neel28iitm <153009818+Neel28iitm@users.noreply.github.com>
2025-04-09 19:21:20 +0530

    Create evaluate.py

---
+++ question_2/evaluate.py
@@ -0,0 +1,13 @@
+import torch
+
+def evaluate(model, val_loader, device):
+    model.eval()
+    total, correct = 0, 0
+    with torch.no_grad():
+        for images, labels in val_loader:
+            images, labels = images.to(device), labels.to(device)
+            outputs = model(images)
+            _, preds = torch.max(outputs, 1)
+            total += labels.size(0)
+            correct += (preds == labels).sum().item()
+    return 100 * correct / total
